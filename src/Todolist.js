c108a484e46647325c260917f9daeea006ec6d79
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2022-12-05 14:01:23 +0530

    final product

---
+++ src/Todolist.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function Todolist(){
+    const [activity, setActivity] = useState("");
+    const [listData, setlistData] = useState([]);
+    function addActivity(){
+            // setlistData([...listData,activity]);
+            // console.log(listData);
+            setlistData((listData) =>{
+                const updatedList = [...listData,activity];
+                console.log(updatedList)
+                setActivity("");
+                return updatedList;
+            })
+    }
+    function removeActivity(i){
+            const updatedListData = listData.filter((elem,id)=>{
+                return i != id;
+            })
+            setlistData(updatedListData);
+    }
+    function removeAll(){
+        setlistData([]);
+    }
+    return(
+        <>
+        <div className='container'>
+            <div className='heading'>Todo List</div>
+            <input type="text" placeholder='add here your activities' value={activity} onChange={(e) => setActivity(e.target.value)} />
+            <button onClick={addActivity}>Add</button>
+            <p className='list-heading'>Here is your List:{")"}</p>
+            {listData !=[] && listData.map((data,i)=>{
+                return(
+                    <>
+                    <p key={i}>
+                        <div className='list-data'>{data}</div>
+                        <div>
+                            <button className='btn-position' onClick={()=>removeActivity(i)}>remove(-)</button>
+                        </div>
+                    </p>
+                    </>
+                )
+            })}
+            {listData.length >=1 && <button onClick={removeAll}>Remove All</button>
+            }
+        </div>
+        </>
+    );
+}
+
+export default Todolist
